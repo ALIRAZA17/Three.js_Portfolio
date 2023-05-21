2cf203935f4d7caa2d4c79831f105d746c6d96fa
Ali Raza <ialidar2001@gmail.com>
2023-05-21 22:55:35 +0500

    Added component testing

--- cypress.config.js
+++ cypress.config.js
@@ -15,18 +15,18 @@ export default defineConfig({
       })
     }
   },
+
   component: {
-    baseUrl: 'http://localhost:5173',
-    setupNodeEvents (on, config) {
-      // implement node event listeners here
-      on('task', {
-        seedDB (filename) {
-          // do anything you want to do such as writing a specific thing to a file etc.
-          // e.g., I print the name of the file that I passed.
-          console.log(filename)
-          return filename
-        }
-      })
+    async devServer ({ specs, cypressConfig, devServerEvents }) {
+      const { port, close } = await startDevServer(
+        specs,
+        cypressConfig,
+        devServerEvents
+      )
+      return {
+        port,
+        close
+      }
     }
   }
 })
