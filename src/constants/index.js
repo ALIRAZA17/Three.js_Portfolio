db48b286979145f7dbed8b452f200bf1f5f312ab
ALIRAZA17 <ialidar2001@gmail.com>
2024-02-17 22:57:21 +0500

    some changes

--- src/constants/index.js
+++ src/constants/index.js
@@ -22,7 +22,6 @@ import {
   upmeal_app,
   talawa,
   instagram,
-  shopApp,
   threejs,
   upmeal,
   GDSC,
@@ -34,365 +33,376 @@ import {
   faizan,
   insta_logo,
   linkedIn_logo,
-  gmail
-} from '../assets'
+  gmail,
+  dodao,
+} from "../assets";
 
 export const navLinks = [
   {
-    id: 'about',
-    title: 'About'
+    id: "about",
+    title: "About",
   },
   {
-    id: 'work',
-    title: 'Work'
+    id: "work",
+    title: "Work",
   },
   {
-    id: 'contact',
-    title: 'Contact'
-  }
-]
+    id: "contact",
+    title: "Contact",
+  },
+];
 
 const services = [
   {
-    title: 'Web Developer',
-    icon: web
+    title: "Web Developer",
+    icon: web,
   },
   {
-    title: 'Mobile Application Developer',
-    icon: mobile
+    title: "Mobile Application Developer",
+    icon: mobile,
   },
   {
-    title: 'Backend Developer',
-    icon: backend
+    title: "Backend Developer",
+    icon: backend,
   },
   {
-    title: 'UI/UX Designer',
-    icon: creator
-  }
-]
+    title: "UI/UX Designer",
+    icon: creator,
+  },
+];
 
 const technologies = [
   {
-    name: 'HTML 5',
-    icon: html
+    name: "HTML 5",
+    icon: html,
   },
   {
-    name: 'CSS 3',
-    icon: css
+    name: "CSS 3",
+    icon: css,
   },
   {
-    name: 'JavaScript',
-    icon: javascript
+    name: "JavaScript",
+    icon: javascript,
   },
   {
-    name: 'Flutter',
-    icon: flutter
+    name: "Flutter",
+    icon: flutter,
   },
   {
-    name: 'React JS',
-    icon: reactjs
+    name: "React JS",
+    icon: reactjs,
   },
   {
-    name: 'Kotlin',
-    icon: kotlin
+    name: "Kotlin",
+    icon: kotlin,
   },
   {
-    name: 'Tailwind CSS',
-    icon: tailwind
+    name: "Tailwind CSS",
+    icon: tailwind,
   },
   {
-    name: 'Node JS',
-    icon: nodejs
+    name: "Node JS",
+    icon: nodejs,
   },
   {
-    name: 'MongoDB',
-    icon: mongodb
+    name: "MongoDB",
+    icon: mongodb,
   },
   {
-    name: 'Three JS',
-    icon: threejs
+    name: "Three JS",
+    icon: threejs,
   },
   {
-    name: 'git',
-    icon: git
+    name: "git",
+    icon: git,
   },
   {
-    name: 'figma',
-    icon: figma
+    name: "figma",
+    icon: figma,
   },
   {
-    name: 'Firebase',
-    icon: firebase
-  }
-]
+    name: "Firebase",
+    icon: firebase,
+  },
+];
 
 const experiences = [
   {
-    title: 'Associate Software Engineer',
-    company_name: 'AEC Softwares',
-    icon: swam_tech,
-    iconBg: '#383E56',
-    date: 'June 2023 - Sep 2023',
+    title: "Full Stack Developer",
+    company_name: "DODAO",
+    icon: dodao,
+    iconBg: "#E6DEDD",
+    date: "Dec 2023 - Present",
     points: [
- 'Developed production-ready mobile applications using Flutter and Swift as an Associate Software Engineer at AEC Software House.',
- 'Played a key role in designing and implementing mobile application features, ensuring functionality met project requirements.',
- 'Collaborated with the development team to maintain code quality and adhere to best practices in mobile application development.',
- 'Worked on both Flutter and Swift, showcasing versatility in mobile development for iOS and Android platforms along with different kinds of providers such as Riverpod.',
- 'Conducted code reviews and actively participated in discussions to improve overall codebase quality and team collaboration.',
-    ]
+      "Spearheaded development on DoDAO's flagship website using Next.js, enhancing user experience and optimizing performance.",
+      "Implemented robust database solutions with Prisma and PostgreSQL.",
+      "Leveraged blockchain concepts to integrate cutting-edge features, enriching the platform's capabilities.",
+    ],
   },
   {
-    title: 'MERN Stack Developer',
-    company_name: 'Shadiyana',
-    icon: shadiyana,
-    iconBg: '#E6DEDD',
-    date: 'July 2023 - Sep 2023',
+    title: "Associate Software Engineer",
+    company_name: "AEC Softwares",
+    icon: swam_tech,
+    iconBg: "#383E56",
+    date: "June 2023 - Sep 2023",
     points: [
-       "Developed Shadiyana's website as a MERN Stack Developer during internship.",
+      "Developed production-ready mobile applications using Flutter and Swift as an Associate Software Engineer at AEC Software House.",
+      "Played a key role in designing and implementing mobile application features, ensuring functionality met project requirements.",
+      "Collaborated with the development team to maintain code quality and adhere to best practices in mobile application development.",
+      "Worked on both Flutter and Swift, showcasing versatility in mobile development for iOS and Android platforms along with different kinds of providers such as Riverpod.",
+    ],
+  },
+  {
+    title: "MERN Stack Developer",
+    company_name: "Shadiyana",
+    icon: shadiyana,
+    iconBg: "#E6DEDD",
+    date: "July 2023 - Sep 2023",
+    points: [
+      "Developed Shadiyana's website as a MERN Stack Developer during internship.",
       "Collaborated with cross-functional teams, including designers and product managers and implemented responsive design practices for optimal user experience",
       "Ensured cross-browser compatibility for widespread accessibility and actively participated in code reviews, providing constructive feedback",
-       "Gained valuable insights into startup dynamics and fast-paced development environment"
-    ]
+      "Gained valuable insights into startup dynamics and fast-paced development environment",
+    ],
   },
   {
-    title: 'Flutter Developer',
-    company_name: 'SWAM Tech',
+    title: "Flutter Developer",
+    company_name: "SWAM Tech",
     icon: swam_tech,
-    iconBg: '#383E56',
-    date: 'June 2022 - Oct 2022',
+    iconBg: "#383E56",
+    date: "June 2022 - Oct 2022",
     points: [
-      'Developing and maintaining mobile applications using Flutter and other related technologies.',
-      'Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.',
-      'Implementing responsive design and ensuring cross-browser compatibility.',
-      'Participating in code reviews and providing constructive feedback to other developers.'
-    ]
+      "Developing and maintaining mobile applications using Flutter and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
   },
   {
-    title: 'MERN Stack Developer',
-    company_name: 'Walee',
+    title: "MERN Stack Developer",
+    company_name: "Walee",
     icon: walee,
-    iconBg: '#E6DEDD',
-    date: 'Nov 2022 - Dec 2022',
+    iconBg: "#E6DEDD",
+    date: "Nov 2022 - Dec 2022",
     points: [
-      'As a MERN stack developer, I worked extensively with React.js and related technologies to build and maintain web applications.',
-      'Collaborating closely with designers, product managers, and other developers, I ensured that our products were of the highest quality and met our clients needs.',
-      'Implementing responsive design and ensuring cross-browser compatibility.'
-    ]
+      "As a MERN stack developer, I worked extensively with React.js and related technologies to build and maintain web applications.",
+      "Collaborating closely with designers, product managers, and other developers, I ensured that our products were of the highest quality and met our clients needs.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+    ],
   },
   {
-    title: 'Mobile Application Lead',
-    company_name: 'GDSC NUST',
+    title: "Mobile Application Lead",
+    company_name: "GDSC NUST",
     icon: GDSC,
-    iconBg: '#383E56',
-    date: 'Nov 2022 - Present',
+    iconBg: "#383E56",
+    date: "Nov 2022 - Present",
     points: [
-      'As the App Lead at GDSC NUST, I have been responsible for overseeing the entire mobile app development lifecycle, from ideation and design to development, testing, and deployment.',
-      'I have worked closely with team members to provide mentorship and guidance, resulting in a high-performing team that consistently delivers top-quality results.',
-      'This has allowed me to develop a comprehensive understanding of mobile app development and management.'
-    ]
+      "As the App Lead at GDSC NUST, I have been responsible for overseeing the entire mobile app development lifecycle, from ideation and design to development, testing, and deployment.",
+      "I have worked closely with team members to provide mentorship and guidance, resulting in a high-performing team that consistently delivers top-quality results.",
+      "This has allowed me to develop a comprehensive understanding of mobile app development and management.",
+    ],
   },
   {
-    title: 'Lead Flutter Developer',
-    company_name: 'Upmeal',
+    title: "Lead Flutter Developer",
+    company_name: "Upmeal",
     icon: upmeal,
-    iconBg: '#E6DEDD',
-    date: 'Jan 2023 - Present',
+    iconBg: "#E6DEDD",
+    date: "Jan 2023 - Present",
     points: [
-      'I have been responsible for leading the development of a discounted fine dining application for users in different restaurants.',
-      'Leveraging my expertise in Flutter, Dart, and related technologies, I have played a key role in the design, development of this innovative product.',
-      'Working closely with cross-functional teams, including designers, product managers, and other developers, I have ensured that the application meets the highest standards of quality and user experience.'
-    ]
-  }
-]
+      "I have been responsible for leading the development of a discounted fine dining application for users in different restaurants.",
+      "Leveraging my expertise in Flutter, Dart, and related technologies, I have played a key role in the design, development of this innovative product.",
+      "Working closely with cross-functional teams, including designers, product managers, and other developers, I have ensured that the application meets the highest standards of quality and user experience.",
+    ],
+  },
+];
 
 const testimonials = [
   {
     testimonial:
-      'As an intern, he was able to showcase his technical expertise and strong communication skills, which were essential to the successful delivery of our MVP.',
-    name: 'Shahryar Khan',
-    designation: 'CEO',
-    company: 'SWAM Tech',
-    image: shahryar
+      "As an intern, he was able to showcase his technical expertise and strong communication skills, which were essential to the successful delivery of our MVP.",
+    name: "Shahryar Khan",
+    designation: "CEO",
+    company: "SWAM Tech",
+    image: shahryar,
   },
   {
     testimonial:
-      'During his tenure with our school, he displayed exceptional professionalism, work ethic, and dedication to his role. Highly recommend her for any future endeavors.',
-    name: 'Dorothea Stubs',
-    designation: 'Head',
-    company: 'Excalibur',
-    image: 'https://randomuser.me/api/portraits/women/6.jpg'
+      "During his tenure with our school, he displayed exceptional professionalism, work ethic, and dedication to his role. Highly recommend her for any future endeavors.",
+    name: "Dorothea Stubs",
+    designation: "Head",
+    company: "Excalibur",
+    image: "https://randomuser.me/api/portraits/women/6.jpg",
   },
   {
     testimonial:
-      'He demonstrated exceptional skills in software development, which were evident in the timely delivery of the Minimum Viable Product (MVP) for the launch.',
-    name: 'Yaruq Nadeem',
-    designation: 'Head',
-    company: 'NSTP',
-    image: yaruq
-  }
-  ,
+      "He demonstrated exceptional skills in software development, which were evident in the timely delivery of the Minimum Viable Product (MVP) for the launch.",
+    name: "Yaruq Nadeem",
+    designation: "Head",
+    company: "NSTP",
+    image: yaruq,
+  },
   {
     testimonial:
-      'I hired Ali raza for my website and he delivered everything requested. Cooperative and friendly.I Recommend.',
-    name: 'Muhammad Hamza',
-    designation: 'CEO',
-    company: 'Graysol',
-    image: hamza
+      "I hired Ali raza for my website and he delivered everything requested. Cooperative and friendly.I Recommend.",
+    name: "Muhammad Hamza",
+    designation: "CEO",
+    company: "Graysol",
+    image: hamza,
   },
   {
     testimonial:
       "I hired Ali for my mobile app, and he delivered everything I asked for. I'm happy with his cooperative and friendly approach. Highly recommended.",
-    name: 'Faizan Naseem',
-    designation: 'President',
-    company: 'CSS GCU',
-    image: faizan
+    name: "Faizan Naseem",
+    designation: "President",
+    company: "CSS GCU",
+    image: faizan,
   },
-]
+];
 
 const projects = [
   {
-    name: 'EaseTour',
+    name: "EaseTour",
     description:
-      'A ride sharing mobile application that allows customers to book rides and have realtime chat and ride visualization features. Users can share rides to share fare among them.',
+      "A ride sharing mobile application that allows customers to book rides and have realtime chat and ride visualization features. Users can share rides to share fare among them.",
     tags: [
       {
-        name: 'Flutter',
-        color: 'blue-text-gradient'
+        name: "Flutter",
+        color: "blue-text-gradient",
       },
       {
-        name: 'Firebase',
-        color: 'green-text-gradient'
+        name: "Firebase",
+        color: "green-text-gradient",
       },
       {
-        name: 'Google Maps API',
-        color: 'green-text-gradient'
-      }
+        name: "Google Maps API",
+        color: "green-text-gradient",
+      },
     ],
     image: easetour,
-    source_code_link: 'https://github.com/ALIRAZA17/EaseTour'
+    source_code_link: "https://github.com/ALIRAZA17/EaseTour",
   },
   {
-    name: 'Jobify',
+    name: "Jobify",
     description:
-      'Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.',
+      "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
     tags: [
       {
-        name: 'JS',
-        color: 'blue-text-gradient'
+        name: "JS",
+        color: "blue-text-gradient",
       },
 
       {
-        name: 'laravel',
-        color: 'green-text-gradient'
+        name: "laravel",
+        color: "green-text-gradient",
       },
       {
-        name: 'tailwind',
-        color: 'pink-text-gradient'
-      }
+        name: "tailwind",
+        color: "pink-text-gradient",
+      },
     ],
     image: jobify,
-    source_code_link: 'https://github.com/ALIRAZA17/web-project'
+    source_code_link: "https://github.com/ALIRAZA17/web-project",
   },
- 
+
   {
-    name: 'Talawa App',
+    name: "Talawa App",
     description:
-      'Open-Source Mobile Application which helps community based organizations collaborate with their members through sharing information, events, and different resources.',
+      "Open-Source Mobile Application which helps community based organizations collaborate with their members through sharing information, events, and different resources.",
     tags: [
       {
-        name: 'Flutter',
-        color: 'blue-text-gradient'
+        name: "Flutter",
+        color: "blue-text-gradient",
       },
       {
-        name: 'GraphQL',
-        color: 'green-text-gradient'
+        name: "GraphQL",
+        color: "green-text-gradient",
       },
       {
-        name: 'MongoDB',
-        color: 'pink-text-gradient'
-      }
+        name: "MongoDB",
+        color: "pink-text-gradient",
+      },
     ],
     image: talawa,
-    source_code_link: 'https://github.com/ALIRAZA17/talawa'
+    source_code_link: "https://github.com/ALIRAZA17/talawa",
   },
   {
-    name: 'Marham',
+    name: "Marham",
     description:
-      'A platform to connect people in need with those who are willing and able to provide assistance.  Specifically designed to help those who are struggling financially.',
+      "A platform to connect people in need with those who are willing and able to provide assistance.  Specifically designed to help those who are struggling financially.",
     tags: [
       {
-        name: 'Flutter',
-        color: 'blue-text-gradient'
+        name: "Flutter",
+        color: "blue-text-gradient",
       },
       {
-        name: 'Firebase',
-        color: 'green-text-gradient'
+        name: "Firebase",
+        color: "green-text-gradient",
       },
       {
-        name: 'GMAPI',
-        color: 'pink-text-gradient'
-      }
+        name: "GMAPI",
+        color: "pink-text-gradient",
+      },
     ],
     image: community,
-    source_code_link: 'https://github.com/ALIRAZA17/Marham'
+    source_code_link: "https://github.com/ALIRAZA17/Marham",
   },
   {
-    name: 'Upmeal',
+    name: "Upmeal",
     description:
-      'A platform where you can avail discounts on every item on the menu. Upmeal will connect hungry tummies to empty tables by providing time-based discounts to users.',
+      "A platform where you can avail discounts on every item on the menu. Upmeal will connect hungry tummies to empty tables by providing time-based discounts to users.",
     tags: [
       {
-        name: 'Flutter',
-        color: 'blue-text-gradient'
+        name: "Flutter",
+        color: "blue-text-gradient",
       },
       {
-        name: 'Nodejs',
-        color: 'green-text-gradient'
+        name: "Nodejs",
+        color: "green-text-gradient",
       },
       {
-        name: 'ML',
-        color: 'pink-text-gradient'
-      }
+        name: "ML",
+        color: "pink-text-gradient",
+      },
     ],
     image: upmeal_app,
-    source_code_link: 'https://github.com/ALIRAZA17/upmeal_app'
+    source_code_link: "https://github.com/ALIRAZA17/upmeal_app",
   },
   {
-    name: 'Instagram Clone',
+    name: "Instagram Clone",
     description:
-      'The mobile application serves as a social media platform that allows users to share photos and videos with their followers, similar to the popular social media app Instagram.',
+      "The mobile application serves as a social media platform that allows users to share photos and videos with their followers, similar to the popular social media app Instagram.",
     tags: [
       {
-        name: 'Flutter',
-        color: 'blue-text-gradient'
+        name: "Flutter",
+        color: "blue-text-gradient",
       },
       {
-        name: 'Firebase',
-        color: 'green-text-gradient'
+        name: "Firebase",
+        color: "green-text-gradient",
       },
       {
-        name: 'restapi',
-        color: 'pink-text-gradient'
-      }
+        name: "restapi",
+        color: "pink-text-gradient",
+      },
     ],
     image: instagram,
-    source_code_link: 'https://github.com/ALIRAZA17/Instagram-clone-Flutter'
-  }
-]
+    source_code_link: "https://github.com/ALIRAZA17/Instagram-clone-Flutter",
+  },
+];
 
 const options = [
   {
     logo: linkedIn_logo,
-    link: 'https://www.linkedin.com/in/ali-raza-296793219/'
+    link: "https://www.linkedin.com/in/ali-raza-296793219/",
   },
   {
     logo: insta_logo,
-    link: 'https://www.instagram.com/m_aliiii_5/'
+    link: "https://www.instagram.com/m_aliiii_5/",
   },
   {
     logo: gmail,
-    link: 'mailto:ialidar2001@gmail.com'
-  }
-]
+    link: "mailto:ialidar2001@gmail.com",
+  },
+];
 
-export { services, technologies, experiences, testimonials, projects, options }
+export { services, technologies, experiences, testimonials, projects, options };
